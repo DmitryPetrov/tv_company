868cbebc9bb010f1abd01f4fc3be045a5ae89641
Dmitry Petrov <dmitrypetrov93@gmail.com>
2019-04-14 18:31:15 +0300

    first commit

---
+++ src/main/java/com/example/name/TvCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvCompanyApplication.class, args);
+	}
+
+}
